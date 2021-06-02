59f943dccfd2b6ddcd35d33ce4cb931ae4868422
Santiago Da Silva <sdasilva@outlook.es>
2021-06-02 18:23:59 +0000

    translator

---
+++ translator.py
@@ -0,0 +1,37 @@
+import os
+from decouple import config
+from ibm_watson import LanguageTranslatorV3
+import json
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from pandas import json_normalize
+
+URL_LT = config('URL_LT')
+APIKEY_LT = config('APIKEY_LT')
+VERSION_LT = config('VERSION_LT')
+
+def englishtofrench(text):
+    '''
+    Translate english text to french
+    '''
+    # text = 'Hello World!'
+    authenticator = IAMAuthenticator(APIKEY_LT)
+    language_translator = LanguageTranslatorV3(version=VERSION_LT, authenticator=authenticator)
+    language_translator.set_service_url(URL_LT)
+
+    json_normalize(language_translator.list_identifiable_languages().get_result(), "languages")
+
+    # Spanish Translation
+    # translation_response = language_translator.translate(text=text, model_id='en-es')
+    # translation=translation_response.get_result()
+    # spanish_translation = translation['translations'][0]['translation']
+
+    # return spanish_translation
+
+    # French Translation
+    translation_response = language_translator.translate(text=text , model_id='en-fr')
+    translation = translation_response.get_result()
+    french_translation = translation['translations'][0]['translation']
+
+    return french_translation
+
+print(englishtofrench('Hello World!'))
