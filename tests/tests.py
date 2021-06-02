8adc02f9eba471fb149770880fa557b764e045fb
Santiago Da Silva <sdasilva@outlook.es>
2021-06-02 20:45:36 +0000

    TestEnglishToGerman

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from translator import englishtofrench
+from translator import englishtofrench, englishtogerman
 
 class TestEnglishToFrench(unittest.TestCase):
     def testNull(self): 
@@ -9,3 +9,11 @@ class TestEnglishToFrench(unittest.TestCase):
         self.assertEqual(englishtofrench('Hello'), 'Bonjour') # test when 'Hello' is given as input the output is 'Bonjour'.
     def testFrench(self): 
         self.assertEqual(englishtofrench('French'), 'Français') # test when 'French' is given as input the output is 'Français'.
+
+class TestEnglishToGerman(unittest.TestCase):
+    def testNull(self): 
+        self.assertEqual(englishtogerman(None), 'text must be provided') # test when None is given as input the output is 'text must be provided'.
+    def testHello(self): 
+        self.assertEqual(englishtogerman('Hello'), 'Hallo') # test when 'Hello' is given as input the output is 'Bonjour'.
+    def testGerman(self): 
+        self.assertEqual(englishtogerman('German'), 'Deutsch') # test when 'French' is given as input the output is 'Français'.
